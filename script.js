ec295419f156942b4becbc75f68899aa67282d0d
JorgeLuisZerpa <jorzersei@gmail.com>
2025-09-04 21:58:42 -0300

    atualizar site

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-alert("olá mundo!")
+// alert("olá mundo!")
 
+// alert("oi")
+// alert("tudo bem?")
+// alert("vocé vem sempre aqui???")
+// alert(" EN LO SIMPLE ESTA LA GRANDEZA!")
+// let idade
 
+// idade = 18
+// alert(idade)
+
+// idade = "pinha"
+// alert(idade)
+// alert = 18.1
+// alert(idade) 
+
+const amor = "meu primeiro amor"
+alert(amor)
+
+let amor2 = "meu segundo amor!"
+alert(amor2)
