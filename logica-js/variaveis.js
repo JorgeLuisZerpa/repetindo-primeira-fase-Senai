90c082e105a4125a0d51a7fd6b32d5d1679594b8
JorgeLuisZerpa <jorzersei@gmail.com>
2025-09-09 22:13:34 -0300

    adiociona variavel js

---
+++ logica-js/variaveis.js
@@ -0,0 +1,23 @@
+//variavel com var
+//var nome = "Jorge" //string
+// var nome = "luis"
+//nome = "luis"
+
+// variavel com let
+//let nome = "Jorge"
+//nome  = "Luis"
+
+//variavel com declarada const
+
+const nome = "Jorge" //string
+const sobrenome = "Zerpa"
+const idade = 34 //number
+const masculino = true //boolean
+
+// console.log("meu nome é "+ nome + " " +sobrenome +  " minha idade é " + idade + " anos" )
+
+//console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} `)
+
+//array
+const frutas = ["laranja", "maçã", "banana"]
+console.log(frutas[0])
