d2655693c86022d379daa646c07ad26ff75c60be
JorgeLuisZerpa <jorzersei@gmail.com>
2025-09-16 22:13:44 -0300

    arrays-objetos

---
+++ logica-js/arrays-objetos.js
@@ -0,0 +1,22 @@
+const alunosDoSenai = [
+    {
+        nome:"joão",
+        email:"joao@gmail.com",
+        matricula:"1234",
+        idade:40
+    },
+    {
+        nome:"maria",
+        email:"maria@gmail.com",
+        matricula:"321",
+        idade:30
+
+    }
+]
+
+console.log(alunosDoSenai)
+console.log(`meu nome é ${alunosDoSenai[1].nome}`)
+console.log(`meu email é ${alunosDoSenai[1].email}`)
+console.log(`minha matricula é ${alunosDoSenai[1].matricula}`)
+console.log(`minha idade é ${alunosDoSenai[1].idade}`)
+
---
+++ logica-js/arrays.js
@@ -0,0 +1,8 @@
+const alunos = ["joão","pedro","larissa","joana"]
+
+const valores = [100, 100.20, 200.30]
+
+console.log(`meu nome é ${alunos[1]}`)
+
+console.log(`a soma dos valores é ${2+3}`)
+console.log(`total ${valores[1] + valores[2]}`)
