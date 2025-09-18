e59957858153976483abe1b34819a8ee65447e34
JorgeLuisZerpa <jorzersei@gmail.com>
2025-09-17 22:25:37 -0300

    novas

---
+++ sq05/ex53.js
@@ -0,0 +1,19 @@
+let placa 
+placa = Number(prompt("qual é o numero da placa?"))
+
+if(placa == 0 || placa ==1){
+    alert("não pode rodar na segunda-feira")
+}else if(placa == 2 || placa == 3){
+if(placa == 2 || placa == 3)
+    alert("não pode rodar na terça-feira")
+    }else if(placa == 4 || placa == 5){
+ alert("não pode rodar na quarta-feira") 
+ }else if(placa == 6 || placa == 7){
+    alert("não pode rodar na quinta-feira")
+ }   else if(placa == 8 || placa == 9){
+    alert("não pode rodar na sexta-feira")
+ }else {
+    alert("erro do sistema, tente novamnte")
+ }
+
+    
