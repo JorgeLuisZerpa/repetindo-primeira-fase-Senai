9d124841d5f4267c24e47d659a8ed02ebaa43e17
JorgeLuisZerpa <jorzersei@gmail.com>
2025-09-29 21:58:21 -0300

    cnta2dor

---
+++ repeticoes/scripts.js
@@ -0,0 +1,14 @@
+// alert ("hello")
+// let acertos = Number(prompt("quantos aceros?"))
+// let valorPorQuestao = 10/16
+// let nota = acertos * valorPorQuestao
+// alert("sua nota é: " + nota )
+
+let contador = 0
+
+
+while(contador < 5){
+    alert("hello")
+    contador = contador + 1
+
+}
